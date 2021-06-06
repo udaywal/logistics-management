a5c2dd3a9242867f8525241bc59611b4f6ee486d
Vaibhav Udaywal <vaibhav.udaywal@otssolutions.com>
2021-06-06 16:07:34 +0530

    home component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
+
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import AllInboxIcon from '@material-ui/icons/AllInbox';
+import PersonIcon from '@material-ui/icons/Person';
+import CreditCardIcon from '@material-ui/icons/CreditCard';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <div>
-            Home
+        <div className="home">
+
+            <div className="home__heading">
+                Welcome back, admin
+            </div>
+            
+            <div className="home__stats">
+                <Link to='/vehicles' className="home__card">
+                        <LocalShippingIcon />
+                        <p>Vehicles</p>
+                </Link>
+                <Link to='/vehicles' className="home__card">
+                    <AllInboxIcon />
+                    <p>Items</p>
+                </Link>
+                <Link to='/vehicles' className="home__card">
+                    <PersonIcon />
+                    <p>Customers</p>
+                </Link>
+                <Link to='/vehicles' className="home__card">
+                    <CreditCardIcon />
+                    <p>Orders</p>
+                </Link>
+            </div>
+
         </div>
     )
 }
